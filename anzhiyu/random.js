38e829a066107f5c7b0448fc37e9ab43c9b41af2
AndersonHJB <cleland1432803776@icloud.com>
2024-08-08 11:13:19 +0800

    Site updated: 2024-08-08 11:13:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/04/hello-world/"];function toRandomPost(){
+var posts=["posts/9776190d.html","posts/4a17b156.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

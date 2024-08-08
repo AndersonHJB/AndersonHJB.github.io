d571115e64119c25446e31afe5fe71c9edb4ddc0
AndersonHJB <cleland1432803776@icloud.com>
2024-08-08 17:34:53 +0800

    Site updated: 2024-08-08 17:34:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/9776190d.html","posts/4a17b156.html"];function toRandomPost(){
+var posts=["posts/4a17b156.html","posts/c0155143.html","posts/9776190d.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

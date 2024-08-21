73a7710a6c4ba8069f5d98ca723db9956b62db30
AndersonHJB <cleland1432803776@icloud.com>
2024-08-21 20:48:00 +0800

    Site updated: 2024-08-21 20:47:59

--- js/main.js
+++ js/main.js
@@ -1800,7 +1800,7 @@ document.addEventListener("DOMContentLoaded", function () {
     anzhiyu.addEventListenerConsoleMusicList(false);
     anzhiyu.initPaginationObserver();
     // 欢迎信息
-    // NaoKuo.setWelcome_info();
+    NaoKuo.setWelcome_info();
 
     setTimeout(() => {
       setInputFocusListener();

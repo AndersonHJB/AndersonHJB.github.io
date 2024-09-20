36ea0f963a3d78a39ceefd7f8af1304f66c3fea7
AndersonHJB <cleland1432803776@icloud.com>
2024-09-20 10:53:34 +0800

    发布修复农历计算——V0.0.3

--- source/static/js/anniversary.js
+++ source/static/js/anniversary.js
@@ -2,23 +2,28 @@
 
 function initializeAnniversary() {
     function LunarDate(Year, Month, Day) {
-      let solar = Lunar.fromYmdHms(Year, Month, Day, 0, 0, 0).getSolar();
-      return new Date(solar.getYear(), solar.getMonth() - 1, solar.getDay())
+      try {
+        let solar = Lunar.fromYmdHms(Year, Month, Day, 0, 0, 0).getSolar();
+        return new Date(solar.getYear(), solar.getMonth() - 1, solar.getDay());
+      } catch (error) {
+        return LunarDate(Year, Month, Day - 1);
+      }
     }
     // 计算两个日期之间的天数差
     function daysBetween(date1, date2) {
       const oneDay = 24 * 60 * 60 * 1000;
       return Math.ceil((date2 - date1) / oneDay);
     }
-  
+    // 剩余天数
     function daysLeft(dateStr, isLunar) {
-      const [Year, Month, Day] = dateStr.split('-').map(Number);
-      const now = new Date();
+      const [Year, Month, Day] = dateStr.split("-").map(Number);
+      let now = new Date();
+      now = new Date(now.getFullYear(), now.getMonth(), now.getDate());
       let anniversaryDate;
       if (isLunar) {
-        anniversaryDate = LunarDate(now.getFullYear(), Month, Day)
+        anniversaryDate = LunarDate(now.getFullYear(), Month, Day);
         if (anniversaryDate < now) {
-          anniversaryDate = LunarDate(now.getFullYear() + 1, Month, Day)
+          anniversaryDate = LunarDate(now.getFullYear() + 1, Month, Day);
         }
       } else {
         anniversaryDate = new Date(now.getFullYear(), Month - 1, Day);
@@ -28,45 +33,39 @@ function initializeAnniversary() {
       }
       return daysBetween(now, anniversaryDate);
     }
-    
+    // 经过天数
     function totalDays(dateStr, isLunar) {
-      const [Year, Month, Day] = dateStr.split('-').map(Number);
-      const now = new Date();
+      const [Year, Month, Day] = dateStr.split("-").map(Number);
+      let now = new Date();
+      now = new Date(now.getFullYear(), now.getMonth(), now.getDate());
       let startDate;
       if (isLunar) {
-        startDate = LunarDate(Year, Month, Day)
+        startDate = LunarDate(Year, Month, Day);
       } else {
         startDate = new Date(Year, Month - 1, Day);
       }
       return daysBetween(startDate, now);
     }
   
-    const countdownElements = document.querySelectorAll('.countdown');
-    const totalDaysElements = document.querySelectorAll('.total-days');
+    const countdownElements = document.querySelectorAll(".countdown");
+    const totalDaysElements = document.querySelectorAll(".total-days");
   
     countdownElements.forEach(function (elem) {
-      const dateStr = elem.getAttribute('data-date');
-      const isLunar = elem.hasAttribute('data-lunar');
-    //   console.log(dateStr);
-    //   console.log(isLunar);
-    //   console.log(elem.getAttribute('data-lunar'));
-    //   console.log(elem);
+      const dateStr = elem.getAttribute("data-date");
+      const isLunar = elem.hasAttribute("data-lunar");
       elem.textContent = daysLeft(dateStr, isLunar);
     });
+  
     totalDaysElements.forEach(function (elem) {
-      const dateStr = elem.getAttribute('data-date');
-      const isLunar = elem.hasAttribute('data-lunar');
+      const dateStr = elem.getAttribute("data-date");
+      const isLunar = elem.hasAttribute("data-lunar");
       elem.textContent = totalDays(dateStr, isLunar);
-    //   console.log(dateStr);
-    //   console.log(isLunar);
-    //   console.log(elem.getAttribute('data-lunar'));
-    //   console.log(elem);
     });
   }
   
   // 初始页面加载
-  document.addEventListener('DOMContentLoaded', initializeAnniversary);
+  document.addEventListener("DOMContentLoaded", initializeAnniversary);
   
   // 适配 pjax
-  document.addEventListener('pjax:complete', initializeAnniversary);
+  document.addEventListener("pjax:complete", initializeAnniversary);
   

fb9a342213f9a2ce7c957b1696d560daf0bcde90
AndersonHJB <AndersonHJB@users.noreply.github.com>
2026-08-11 21:59:21 +0800

    更新学习方法章节，增加个人经历和有效学习的关键要素，提升内容深度和可读性

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -103,7 +103,6 @@ export const zhNavbar = navbar([
       //     { text: "简书", link: "https://www.jianshu.com/u/7def888b8c12"},
       //     { text: "墨天轮", link: "https://www.modb.pro/u/482441"},
           // { text: "留言", link: "https://comment.bornforthis.cn/"},
-          // { text: "悦兮小栈", link: "https://comment.blovey.art/"},
           // { text: "评论管理", link: "https://comment.bornforthis.cn/ui"},
           // { text: "Stackoverflow", link: "https://stackoverflow.com/users/19551173/ai%e6%82%a6%e5%88%9b"},
           // { text: "Mdnice", link: "https://mdnice.com/user/417729638227"},
